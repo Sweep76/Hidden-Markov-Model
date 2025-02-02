346620c20006269a5342822ab3bd3b2f3393bc78
Sweep <joshuapatrickchiu76@gmail.com>
2025-02-02 22:12:14 +0800

    preprocess transfer

--- run.py
+++ run.py
@@ -1,10 +1,15 @@
 # -*- coding: utf-8 -*-
+
 import argparse
 from datetime import datetime, timedelta
+
 import numpy as np
+
 from config import Config
-from hmm import HMM, preprocess
-# Parse command arguments
+from corpus import Corpus
+from hmm import HMM
+
+
 parser = argparse.ArgumentParser(
     description='Create Hidden Markov Model(HMM) for POS Tagging.'
 )
@@ -13,24 +18,32 @@ parser.add_argument('-b', action='store_true', default=False,
 parser.add_argument('--file', '-f', action='store', dest='file',
                     help='set where to store the model')
 args = parser.parse_args()
-# Load data
+
+
 config = Config(args.bigdata)
-train = preprocess(config.ftrain)
-dev = preprocess(config.fdev)
+
+print("Preprocessing the data")
+corpus = Corpus(config.ftrain)
+train_data = corpus.load(config.ftrain)
+dev_data = corpus.load(config.fdev)
 file = args.file if args.file else config.hmmpkl
-wordseqs, tagseqs = zip(*train)
-words, tags = sorted(set(np.hstack(wordseqs))), sorted(set(np.hstack(tagseqs)))
+
 start = datetime.now()
-print("Creating HMM with %d words and %d tags" % (len(words), len(tags)))
-hmm = HMM(words, tags)
-print("Using %d sentences to train the HMM" % (len(train)))
-hmm.train(train, file)
+
+print("Creating HMM with %d words and %d tags" % (corpus.nw, corpus.nt))
+hmm = HMM(corpus.nw, corpus.nt)
+
+print("Using %d sentences to train the HMM" % (len(corpus.sentences)))
+hmm.train(train_data, file)
+
 print("Using Viterbi algorithm to tag the dataset")
-tp, total, precision = hmm.evaluate(dev)
+tp, total, precision = hmm.evaluate(dev_data)
 print("Precision of dev: %d / %d = %4f" % (tp, total, precision))
+
 if args.bigdata:
-    test = preprocess(config.ftest)
     hmm = HMM.load(file)
-    tp, total, precision = hmm.evaluate(test)
+    test_data = corpus.load(config.ftest)
+    tp, total, precision = hmm.evaluate(test_data)
     print("Precision of test: %d / %d = %4f" % (tp, total, precision))
+
 print("%ss elapsed\n" % (datetime.now() - start))
