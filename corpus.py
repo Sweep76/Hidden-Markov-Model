8e8193158c5d423b0d1343758e3ce30d552a23a3
Sweep <joshuapatrickchiu76@gmail.com>
2025-02-08 10:19:57 +0800

    make preprocess func static

--- corpus.py
+++ corpus.py
@@ -1,19 +1,40 @@
 # -*- coding: utf-8 -*-
+
 import numpy as np
+
+
 class Corpus(object):
     UNK = '<UNK>'
+
     def __init__(self, fdata):
         self.sentences = self.preprocess(fdata)
         self.wordseqs, self.tagseqs = zip(*self.sentences)
         self.words = sorted(set(np.hstack(self.wordseqs)))
         self.tags = sorted(set(np.hstack(self.tagseqs)))
         self.words.append(self.UNK)
+
         self.wdict = {w: i for i, w in enumerate(self.words)}
         self.tdict = {t: i for i, t in enumerate(self.tags)}
         self.ui = self.wdict[self.UNK]
         self.nw = len(self.words)
         self.nt = len(self.tags)
-    def preprocess(self, fdata):
+
+    def load(self, fdata):
+        data = []
+        sentences = self.preprocess(fdata)
+
+        for wordseq, tagseq in sentences:
+            wis = [self.wdict[w] if w in self.wdict else self.ui
+                   for w in wordseq]
+            eis = [self.tdict[t] for t in tagseq]
+            data.append((wis, eis))
+        return data
+
+    def size(self):
+        return self.nw - 1, self.nt
+
+    @staticmethod
+    def preprocess(fdata):
         start = 0
         sentences = []
         with open(fdata, 'r') as train:
@@ -27,14 +48,3 @@ class Corpus(object):
                     start += 1
                 sentences.append((wordseq, tagseq))
         return sentences
-    def load(self, fdata):
-        data = []
-        sentences = self.preprocess(fdata)
-        for wordseq, tagseq in sentences:
-            wis = [self.wdict[w] if w in self.wdict else self.ui
-                   for w in wordseq]
-            eis = [self.tdict[t] for t in tagseq]
-            data.append((wis, eis))
-        return data
-    def size(self):
-        return self.nw - 1, self.nt
